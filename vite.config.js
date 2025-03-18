5a8dbac79dd53ac61f4a6aca330ab8273c9af1d2
valmo-dev <valmo.dev@gmail.com>
2025-03-18 09:18:26 +0100

    feat: mise à jour des dépendances et intégration de DaisyUI

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
         vue({
@@ -17,5 +18,6 @@ export default defineConfig({
                 },
             },
         }),
+        tailwindcss(),
     ],
 });
